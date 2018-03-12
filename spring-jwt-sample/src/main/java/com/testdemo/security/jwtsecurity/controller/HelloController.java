dbb2646e16997538415fdf16ed7b6eb11266bfe9
vikram vyawahare <vikram.vyawahare@walgreens.com>
2018-03-12 16:47:48 -0500

    updated

--- spring-jwt-sample/src/main/java/com/testdemo/security/jwtsecurity/controller/HelloController.java
+++ spring-jwt-sample/src/main/java/com/testdemo/security/jwtsecurity/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.testdemo.security.jwtsecurity.controller;
 
 
+import javax.validation.Valid;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +17,16 @@ public class HelloController {
     @GetMapping
     public String hello() {
         return "Hello World Payload Sample";
+ 
     }
+    
+
+	@RequestMapping(method=RequestMethod.POST, value="/rest/message")
+	public String addMessage()
+	{
+		return "Hello Message";
+	}
+    
 }
+
+
