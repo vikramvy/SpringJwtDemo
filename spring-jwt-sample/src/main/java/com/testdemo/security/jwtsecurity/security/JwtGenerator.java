483d73119675a2db4b0e476d6a294223d7bff0e2
vikram vyawahare <vikram.vyawahare@walgreens.com>
2018-03-19 14:36:28 -0500

    set token expiry date

--- spring-jwt-sample/src/main/java/com/testdemo/security/jwtsecurity/security/JwtGenerator.java
+++ spring-jwt-sample/src/main/java/com/testdemo/security/jwtsecurity/security/JwtGenerator.java
@@ -1,10 +1,16 @@
 package com.testdemo.security.jwtsecurity.security;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
 import com.testdemo.security.jwtsecurity.model.JwtUser;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.stereotype.Component;
 
 @Component
 public class JwtGenerator {
@@ -17,10 +23,21 @@ public class JwtGenerator {
                 .setSubject(jwtUser.getUserName());
         claims.put("userId", String.valueOf(jwtUser.getId()));
         claims.put("role", jwtUser.getRole());
-
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+    	String dateInString = "19-03-2018 11:00:00";
+    	Date expiryDate = null;
+		try {
+			expiryDate = sdf.parse(dateInString);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error parsing Date ");
+			e.printStackTrace();
+			
+		}
 
         return Jwts.builder()
                 .setClaims(claims)
+                .setIssuedAt(new Date()).setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS512, "tempKey")
                 .compact();
     }
